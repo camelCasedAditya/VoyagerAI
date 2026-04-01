cfbb8accb77322548107933703032b752d99b26e
Aditya C <115904204+camelCasedAditya@users.noreply.github.com>
2026-04-01 16:57:22 +0000

    yelp tool working with AI agent

--- agent/main.py
+++ agent/main.py
@@ -3,6 +3,7 @@ import os
 from langchain.agents import create_agent
 from langchain_cerebras import ChatCerebras
 from utils.hotel import get_hotels, geocode_distance_calculator
+from utils.yelp import search_yelp
 
 api_key = os.getenv("CEREBRAS_API_KEY", "csk-t5cdem3w8w4hepvkderrd8jjf6893nnh9efmhhv8yv3fwdjd")
 model_name = os.getenv("CEREBRAS_MODEL", "qwen-3-235b-a22b-instruct-2507")
@@ -11,15 +12,15 @@ llm = ChatCerebras(model=model_name, api_key=api_key)
 
 agent = create_agent(
     llm, 
-    tools=[get_hotels, geocode_distance_calculator],
-    system_prompt="You are a helpful travel agent that finds hotels for customers. Use the get_hotels function to find hotels based on the customer's query. Use yhe geocode_distance_calculator function to calculate distances between the hotels and the customer's desired location. Pick the top three hotels for the customer based on pricing, location, and reputation"
+    tools=[get_hotels, geocode_distance_calculator, search_yelp],
+    system_prompt="You are a helpful travel agent that finds hotels for customers. Use the get_hotels function to find hotels based on the customer's query. Use the geocode_distance_calculator function to calculate distances between the hotels and the customer's desired location. Pick the top three hotels for the customer based on pricing, location, and reputation. Then use the search_yelp tool to find restaurants near the hotels for the customer. Provide the customer with a list of the top three hotels and nearby restaurants based on their query. Always use the tools provided to you to find the best hotels and restaurants for the customer. If you don't have enough information to answer the customer's query, ask them for more information."
     )
 response = agent.invoke(
     {
         "messages": [
             {
                 "role": "user",
-                "content": "Find me hotels near 1600 Amphitheatre Parkway, Mountain View, CA for 2 adults checking in on May 5th, 2026 and checking out on May 7th, 2026. I need 1 room.",
+                "content": "Plan a trip for near 1600 Amphitheatre Parkway, Mountain View, CA for 2 adults checking in on May 5th, 2026 and checking out on May 7th, 2026. I need 1 room. Also find me the three best restaurants and food locations",
             }
         ]
     }
--- agent/utils/yelp.py
+++ agent/utils/yelp.py
@@ -2,9 +2,14 @@ import requests
 import os
 from dotenv import load_dotenv
 import json
+from langchain_core.tools import tool
 load_dotenv()
 
-def search_yelp(term, lat, lng, radius, limit, price, time, number_of_people, date, categories=None):
+@tool("search_yelp")
+def search_yelp(lat: float, lng: float, radius: int, limit: int, price: str, time: str, number_of_people: int, date: str, categories=None):
+    """Search for businesses on Yelp. Takes the parameters lat: float, lng: float, radius: int, limit: int, price: str (e.g., "1,2,3,4" or "1,3" with 1 being the lowest price tier and 4 being the highest), time: str (e.g., "19:00" format is HH:MM), number_of_people: int, date: str (e.g., "2023-12-31"). Returns a list of businesses that match the search criteria."""
+
+    print(f"Called search_yelp with lat: {lat}, lng: {lng}, radius: {radius}, limit: {limit}, price: {price}, time: {time}, number_of_people: {number_of_people}, date: {date}, categories: {categories}")
     api_key = os.getenv("YELP_API_KEY")
     url = "https://api.yelp.com/v3/businesses/search"
 
@@ -19,7 +24,7 @@ def search_yelp(term, lat, lng, radius, limit, price, time, number_of_people, da
     query_params = {
         "latitude": lat,
         "longitude": lng,
-        "term": f"{term}",
+        "term": "food",
         "radius": radius,
         "categories": categories_param,
         "locale": "en_US",
@@ -34,8 +39,8 @@ def search_yelp(term, lat, lng, radius, limit, price, time, number_of_people, da
     }
 
     response = requests.get(url, headers=headers, params=query_params)
-    with open("yelp_response.json", "w") as f:
-        f.write(json.dumps(response.json(), indent=4))
+    # with open("yelp_response.json", "w") as f:
+    #     f.write(json.dumps(response.json(), indent=4))
 
-    print(response.text)
-search_yelp(term="sushi", lat=37.7749, lng=-122.4194, radius=4000, limit=20, price="1,2", time="19:00", number_of_people=2, date="2023-12-31")
+    return f"Here is a list of restaurants and food locations for the specified area: {json.dumps(response.json(), indent=4)}"
+# search_yelp(term="sushi", lat=37.7749, lng=-122.4194, radius=4000, limit=20, price="1,2", time="19:00", number_of_people=2, date="2023-12-31")
